9f7180fd571027a5af719266579ef4cd186a2fb2
Gevorg Davoian <gdavoian@gmail.com>
2020-03-17 11:49:42 +0200

    Cleanup

--- api/mappings.py
+++ api/mappings.py
@@ -25,17 +25,27 @@ class Indicator(Mapping):
         indicator = cls.DEFAULTS.copy()
 
         indicator['id'] = f'transient:{uuid.uuid4()}'
+
+        # `BreachDate` itself is just a date with no time (i.e. YYYY-MM-DD),
+        # so make sure to add some time to make the date comply with ISO 8601.
         indicator['valid_time'] = {
             'start_time': breach['BreachDate'] + 'T00:00:00Z'
         }
+
         indicator['confidence'] = ['Medium', 'High'][breach['IsVerified']]
+
         indicator['description'] = breach['Description']
+
         indicator['severity'] = ['Medium', 'High'][
             breach['IsVerified'] and 'Passwords' in breach['DataClasses']
         ]
+
         indicator['short_description'] = breach['Title']
+
         indicator['tags'] = breach['DataClasses']
+
         indicator['timestamp'] = breach['ModifiedDate']
+
         indicator['title'] = breach['Name']
 
         return indicator
