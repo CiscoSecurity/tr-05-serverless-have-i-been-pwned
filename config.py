c33fbcd23dd8b5b93d1c4d0cef6b52dfba674c6f
Gevorg Davoian <gdavoian@gmail.com>
2020-04-26 13:38:33 +0300

    Introduce CTR_ENTITIES_LIMIT

--- config.py
+++ config.py
@@ -15,6 +15,14 @@ class Config:
         '(tr-integrations-support@cisco.com)'
     )
 
+    CTR_ENTITIES_LIMIT_DEFAULT = 100
+
+    try:
+        CTR_ENTITIES_LIMIT = int(os.environ['CTR_ENTITIES_LIMIT'])
+        assert CTR_ENTITIES_LIMIT > 0
+    except (KeyError, ValueError, AssertionError):
+        CTR_ENTITIES_LIMIT = CTR_ENTITIES_LIMIT_DEFAULT
+
     HIBP_TEST_EMAIL = 'user@example.com'
 
     HIBP_API_URL = (
